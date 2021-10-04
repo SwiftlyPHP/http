f156ce4a7be3e49a8f8d6fd60d047a18a77a2b0d
clvarley <conorlukevarley@gmail.com>
2021-10-04 18:17:35 +0100

    Simple tests for the authenticators

--- tests/Authentication/BasicAuthenticatorTest.php
+++ tests/Authentication/BasicAuthenticatorTest.php
@@ -3,7 +3,10 @@
 namespace Swiftly\Http\Tests\Authentication;
 
 use Swiftly\Http\Authentication\BasicAuthenticator;
+use Swiftly\Http\Client\Request;
 use PHPUnit\Framework\TestCase;
+
+use function base64_encode;
 
 /**
  * @group Shared
@@ -11,4 +14,27 @@ use PHPUnit\Framework\TestCase;
 Class BasicAuthenticatorTest Extends TestCase
 {
 
+    /** @var BasicAuthenticator $authenticator */
+    private $authenticator;
+
+    /** @var Request $request */
+    private $request;
+
+    protected function setUp() : void
+    {
+        $this->authenticator = new BasicAuthenticator( 'username', 'password' );
+        $this->request = new Request();
+    }
+
+    public function testSetsCorrectHttpHeader() : void
+    {
+        $this->authenticator->authenticate( $this->request );
+
+        $expected = base64_encode( 'username:password' );
+
+        self::assertTrue( $this->request->headers->has( 'Authorization' ) );
+        self::assertSame( "Basic $expected",
+            $this->request->headers->get( 'Authorization' )
+        );
+    }
 }
--- tests/Authentication/BearerAuthenticatorTest.php
+++ tests/Authentication/BearerAuthenticatorTest.php
@@ -3,6 +3,7 @@
 namespace Swiftly\Http\Tests\Authentication;
 
 use Swiftly\Http\Authentication\BearerAuthenticator;
+use Swiftly\Http\Client\Request;
 use PHPUnit\Framework\TestCase;
 
 /**
@@ -11,4 +12,25 @@ use PHPUnit\Framework\TestCase;
 Class BearerAuthenticatorTest Extends TestCase
 {
 
+    /** @var BearerAuthenticator $authenticator */
+    private $authenticator;
+
+    /** @var Request $request */
+    private $request;
+
+    protected function setUp() : void
+    {
+        $this->authenticator = new BearerAuthenticator( 'some_api_token' );
+        $this->request = new Request();
+    }
+
+    public function testSetsCorrectHttpHeader() : void
+    {
+        $this->authenticator->authenticate( $this->request );
+
+        self::assertTrue( $this->request->headers->has( 'Authorization' ) );
+        self::assertSame( "Bearer some_api_token",
+            $this->request->headers->get( 'Authorization' )
+        );
+    }
 }
