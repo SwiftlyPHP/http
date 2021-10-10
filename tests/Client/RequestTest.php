39176cf03c08efb28978af6084e769722c9e4cc1
clvarley <conorlukevarley@gmail.com>
2021-10-10 02:26:39 +0100

    Stubbed out tests for client request class

--- tests/Client/RequestTest.php
+++ tests/Client/RequestTest.php
@@ -3,6 +3,7 @@
 namespace Swiftly\Http\Tests\Client;
 
 use Swiftly\Http\Client\Request;
+use Swiftly\Http\Client\Response;
 use PHPUnit\Framework\TestCase;
 
 /**
@@ -11,4 +12,29 @@ use PHPUnit\Framework\TestCase;
 Class RequestTest Extends TestCase
 {
 
+    /** @var Request $request */
+    private $request;
+
+    protected function setUp() : void
+    {
+        $this->request = new Request();
+    }
+
+    public function testCanMakeGetRequest() : void
+    {
+        $response = $this->request->get( 'http://localhost' );
+
+        self::assertInstanceOf( Response::class, $response );
+
+        // TODO
+    }
+
+    public function testCanMakePostRequest() : void
+    {
+        $response = $this->request->post( 'http://localhost' );
+
+        self::assertInstanceOf( Response::class, $response );
+
+        // TODO
+    }
 }
